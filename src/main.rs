7be5d505af36122afd61e66827b11a6612e3f45e
Sylvan B <sylvanbowdler18@gmail.com>
2021-02-17 22:30:27 +0000

    Multipart uploads implemented

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use std::fs::{self, File};
+use std::{
+    ffi::OsStr,
+    fs::{self, File},
+    io::Write,
+    path::Path,
+};
 
 use nanoid::nanoid;
 use pickledb::{PickleDb, PickleDbDumpPolicy, SerializationMethod};
 use rocket::{
-    http::{hyper::StatusCode, ContentType},
-    response::{Content, Stream},
-    Data,
     get,
+    http::{hyper::StatusCode, ContentType},
     post,
-    routes
+    response::{Content, Stream},
+    routes, Data,
 };
+use rocket_multipart_form_data::MultipartFormDataOptions;
+use rocket_multipart_form_data::{MultipartFormData, MultipartFormDataField};
 
 #[get("/")]
 fn index() -> Result<Content<String>, StatusCode> {
@@ -35,41 +41,66 @@ fn get_file(id: String) -> Option<Stream<File>> {
     None
 }
 
-#[post("/upload", format = "image/png", data = "<data>")]
-fn upload_png(data: Data) -> Result<Content<String>, std::io::Error> {
-    let id = nanoid!();
-    Ok(save_image_stream(data, id))
+#[post("/upload", data = "<data>")]
+fn multipart_upload(content_type: &ContentType, data: Data) -> Result<String, &str> {
+    let options = MultipartFormDataOptions::with_multipart_form_data_fields(vec![
+        MultipartFormDataField::raw("file").size_limit(32 * 1042 * 1024),
+    ]);
+
+    let mut multipart_form_data = match MultipartFormData::parse(content_type, data, options) {
+        Ok(form_data) => form_data,
+        Err(err) => match err {
+            rocket_multipart_form_data::MultipartFormDataError::DataTooLargeError(_) => {
+                return Err("File is too large")
+            }
+            rocket_multipart_form_data::MultipartFormDataError::DataTypeError(_) => {
+                return Err("Incorrect content type");
+            }
+            _ => panic!("{:?}", err),
+        },
+    };
+
+    let file = multipart_form_data.raw.remove("file");
+
+    match file {
+        Some(mut file) => {
+            let raw = file.remove(0);
+            let file_name = raw.file_name.unwrap();
+            let extension = Path::new(&file_name)
+                .extension()
+                .and_then(OsStr::to_str)
+                .unwrap();
+
+            let id = nanoid!();
+            let file_name = String::from(format!("{}.{}", id, extension));
+            let data = raw.raw;
+
+            save_file(data, &file_name, &id).or(Err("Failed to save file"))
+        }
+        None => Err("Please input a file"),
+    }
 }
 
-#[post("/upload", format = "image/jpeg", data = "<data>")]
-fn upload_jpeg(data: Data) -> Result<Content<String>, std::io::Error> {
-    let id = nanoid!();
-    Ok(save_image_stream(data, id))
-}
-
-#[post("/upload", format = "plain", data = "<data>")]
-fn upload_text(data: Data) -> Result<Content<String>, std::io::Error> {
-    let id = nanoid!();
-    Ok(save_image_stream(data, id))
-}
-
-fn save_image_stream(data: Data, id: String) -> Content<String> {
+fn save_file(data: Vec<u8>, file_name: &String, id: &String) -> Result<String, &'static str> {
     let mut db = PickleDb::new(
         "data.db",
         PickleDbDumpPolicy::AutoDump,
         SerializationMethod::Json,
     );
 
-    match data.stream_to_file(format!("{}", id)) {
+    let mut file = File::create(file_name).or_else(|_| return Err("Failed to create file"))?;
+
+    match file.write_all(&data) {
         Ok(_) => {
-            match db.set(&id, &id) {
+            match db.set(&id, &file_name) {
                 Ok(_) => {}
-                Err(_) => return Content(ContentType::HTML, String::from("Failed to save image.")),
+                Err(_) => return Err("Failed to store image id in database"),
             };
-            Content(ContentType::HTML, id)
         }
-        Err(_) => Content(ContentType::HTML, String::from("Failed to save image.")),
-    }
+        Err(_) => return Err("Failed to write data to disk"),
+    };
+
+    Ok(String::from(format!("localhost:8000/file/{}", file_name)))
 }
 
 fn get_index() -> Option<String> {
@@ -77,7 +108,7 @@ fn get_index() -> Option<String> {
 }
 
 fn main() {
-    let routes = routes![index, get_file, upload_png, upload_jpeg, upload_text];
+    let routes = routes![index, get_file, multipart_upload];
 
     rocket::ignite().mount("/", routes).launch();
 }
